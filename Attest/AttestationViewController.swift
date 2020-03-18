31aaadeea1da5f54bf3cddb82813bc00e3744320
Romain Cayzac <cynaptique@gmail.com>
2020-03-18 12:41:24 +0100

    Initial commit

---
+++ Attest/AttestationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AttestationController.swift
+//  Attestation Déplacement
+//
+//  Created by Dev on 18/03/2020.
+//  Copyright © 2020 Dev. All rights reserved.
+//
+
+import UIKit
+
+class AttestionViewController: UIViewController {
+    
+    @IBOutlet let attestationTableView: UITableView?
+    @IBOutlet let profilePickerView: UIPickerView?
+    @IBOutlet let informationButton: UIButton?
+    @IBOutlet let createProfileButton: UIButton?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Attest/IntroductionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Attestation Déplacement
+//
+//  Created by Dev on 18/03/2020.
+//  Copyright © 2020 Dev. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
